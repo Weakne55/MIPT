b2a1bb33e255905af5974d396f2b7eb792e55893
Bogdan Karpovich <karpovich.bv@phystech.edu>
2021-09-08 23:51:50 +0300

    sm

---
+++ Elementary.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sys
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+
+x = int
+n = np.array([], int)
+if b//a == 0:
+    print(-b//a)
+if d//c == 0:
+    print('Error')
+    sys.exit("ZeroErrorDivision")
--- Training.py
+++ Training.py
@@ -1,2 +1,16 @@
-a = [int(i) for i in input().split()]
-b = int(input())
+lst = [0, 1, 2, 3, 4, 5, 6]
+
+def modify_list(l):
+    for item in l:
+        if item % 2 != 0:
+            l.remove(item)
+    for i in range(len(l)):
+        l[i] = l[i]//2
+
+
+
+
+print(modify_list(lst))  # None
+print(lst)               # [1, 2, 3]
+modify_list(lst)
+print(lst)               #[1]
