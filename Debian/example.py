47d7789269bdde8fc14c67ee4d38b450bf59c0cb
Bogdan <karpovich.bv@phystech.edu>
2022-09-02 23:24:17 +0300

    Made a task about the namespace

--- Debian/example.py
+++ Debian/example.py
@@ -1,3 +1,36 @@
-print('hello')
-print(input())
+def create(namespace, parent):
+    scopes[namespace] = {'parent':parent,'variables':set()}
+
+
+def add(namespace, var):
+    scopes[namespace]['variables'].add(var)
+
+
+def get(namespace, var):
+    if namespace == None:
+        print(None)
+        return
+    if var in scopes[namespace]['variables']:
+        print(namespace)
+        return
+    else:
+         get(scopes[namespace]['parent'],var)
+
+scopes = {'global':{'parent':None,'variables':set()}}
+
+n = int(input())
+
+for i in range(n):
+
+    command, name, v = input().split()
+
+    if command == 'create':
+        create(name, v)
+
+    elif command == 'add':
+        add(name, v)    
+
+    elif command == 'get':
+        get(name, v)
+
 
